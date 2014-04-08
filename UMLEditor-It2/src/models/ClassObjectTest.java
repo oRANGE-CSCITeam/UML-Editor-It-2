0f25ff33b09249fe7f8aa142928288eefe6ca054
Jake <ArcherJake@Gmail.com>
2014-04-08 11:44:33 -0400

    TEST

--- UMLEditor-It2/src/models/ClassObjectTest.java
+++ UMLEditor-It2/src/models/ClassObjectTest.java
@@ -11,48 +11,104 @@ public class ClassObjectTest {
 		ClassObject target = new ClassObject("Name", 10, 10, 0);
 		assertEquals("Name", target.getName());
 		assertEquals(10, target.getxPos());
-		fail("Not yet implemented");
+		assertEquals(10, target.getyPos());
+		assertEquals(0, target.getId());
+		assertEquals(0, target.getAttributes().size());
+		assertEquals(0, target.getOperations().size());
+		
 	}
 	
 
 	@Test
 	public void testAddAttribute() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addAttribute("Atrib1", 0);
+		assertEquals(1, target.getAttributes().size());
+		target.addAttribute("Atrib2", 0);
+		target.addAttribute("Atrib3", 0);
+		target.addAttribute("Atrib4", 0);
+		assertEquals(4, target.getAttributes().size());
 	}
 
 	@Test
 	public void testRemoveAttribute() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addAttribute("Atrib1", 0);
+		target.addAttribute("Atrib2", 0);
+		target.addAttribute("Atrib3", 0);
+		target.addAttribute("Atrib4", 0);
+		assertEquals(4, target.getAttributes().size());
+		target.removeAttribute(3);
+		assertEquals(3, target.getAttributes().size());
 	}
 
 	@Test
 	public void testRemoveAllAtributes() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addAttribute("Atrib1", 0);
+		target.addAttribute("Atrib2", 0);
+		target.addAttribute("Atrib3", 0);
+		target.addAttribute("Atrib4", 0);
+		assertEquals(4, target.getAttributes().size());
+		target.removeAllAtributes();
+		assertEquals(0, target.getAttributes().size());
 	}
 
 	@Test
 	public void testAddOperation() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addOperation("Oper1", 0);
+		assertEquals(1, target.getOperations().size());
+		target.addOperation("Oper2", 0);
+		target.addOperation("Oper3", 0);
+		target.addOperation("Oper4", 0);
+		assertEquals(4, target.getOperations().size());
 	}
 
 	@Test
 	public void testRemoveOperation() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addOperation("Oper1", 0);
+		assertEquals(1, target.getOperations().size());
+		target.addOperation("Oper2", 0);
+		target.addOperation("Oper3", 0);
+		target.addOperation("Oper4", 0);
+		assertEquals(4, target.getOperations().size());
+		target.removeOperation(3);
+		assertEquals(3, target.getOperations().size());
 	}
 
 	@Test
 	public void testRemoveAllOperations() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addOperation("Oper1", 0);
+		assertEquals(1, target.getOperations().size());
+		target.addOperation("Oper2", 0);
+		target.addOperation("Oper3", 0);
+		target.addOperation("Oper4", 0);
+		assertEquals(4, target.getOperations().size());
+		target.removeAllOperations();
+		assertEquals(0, target.getOperations().size());
 	}
 
 	@Test
 	public void testGetLongestAttribute() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addAttribute("Atrib1", 0);
+		target.addAttribute("Atrib2", 0);
+		target.addAttribute("Atrib3", 0);
+		target.addAttribute("LongAtrib", 0);
+		assertEquals(9, target.getLongestAttribute());
 	}
 
 	@Test
 	public void testGetLongestOperation() {
-		fail("Not yet implemented");
+		ClassObject target = new ClassObject("Name", 10, 10, 0);
+		target.addOperation("Oper1", 0);
+		target.addOperation("Oper2", 0);
+		target.addOperation("Oper3", 0);
+		target.addOperation("LongOper", 0);
+		assertEquals(8, target.getLongestOperation());
 	}
 
 }
